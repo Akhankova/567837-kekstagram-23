c47fa995c6040effa85437f07cc8fa739ea70081
Alina Akhankova <a.akhankova@gmail.com>
2021-05-29 22:16:19 +0200

    Доработка замечаний наставника

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@ const stringLength = 140;
 
 const getLengthRow = function (row, maxLength) {
 
-  if (row.length<=maxLength) {
+  if (row.length <= maxLength) {
     return true;
   }
   return false;
 };
 getLengthRow(ourString, stringLength);
 
+const invalidValue = 'Максимальное значение не должно быть меньше или равно минимальному значению';
+const negativeValue = 'Диапазон может быть только положительный';
+
 const getRandomValue = function (minValue, maxValue) {
-  const invalidValue = 'Максимальное значение не должно быть меньше или равно минимальному значению';
-  const negativeValue = 'Диапазон может быть только положительный';
-  if (minValue>=maxValue) {
+  if (minValue >= maxValue) {
     return invalidValue;
   }
-  if (minValue<0 || maxValue<0) {
+  if (minValue < 0 || maxValue < 0) {
     return negativeValue;
   }
-  return Math.floor(Math.random()*(maxValue-minValue+1)+minValue);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 };
 getRandomValue();
