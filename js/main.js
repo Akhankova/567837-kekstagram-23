dd2005782a499dd544e25ba3a206ee07f22b1110
Alina Akhankova <a.akhankova@gmail.com>
2021-06-06 17:15:59 +0200

    I corrected errors after the first check

--- js/main.js
+++ js/main.js
@@ -1,57 +1,25 @@
-const ourString = 'проверка работы функции';
-const stringLength = 140;
-
-const getLengthRow = function (row, maxLength) {
-
-  if (row.length <= maxLength) {
-    return true;
-  }
-  return false;
-};
-getLengthRow(ourString, stringLength);
-
-const invalidValue = 'Максимальное значение не должно быть меньше или равно минимальному значению';
-const negativeValue = 'Диапазон может быть только положительный';
-
-const getRandomValue = function (minValue, maxValue) {
-  if (minValue >= maxValue) {
-    return invalidValue;
-  }
-  if (minValue < 0 || maxValue < 0) {
-    return negativeValue;
-  }
-  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
-};
-
 const QUANTITY_GENERATED_OBJECTS = 25;
-const QUANTITY_ID = 25;
-const QUANTITY_URL = 25;
 const MIN_NUMBER_LIKE = 15;
 const MAX_NUMBER_LIKE = 200;
-const QUANTITY_AVATAR_FOTOS = 6;
 const MIN_COMMENT_ID = 1;
 const MAX_COMMENT_ID = 1000;
 const MIN_QUANTITY_COMMENTS = 1;
 const MAX_QUANTITY_COMMENTS = 2;
 
-const descriptionId = [];
-for (let index = 1; index <= QUANTITY_ID; index ++) {
-  descriptionId.push(index);
-}
-const descriptionUrl = [];
-for (let index = 1; index <= QUANTITY_URL; index ++) {
-  descriptionUrl.push(index);
-}
+const ourString = 'проверка работы функции';
+const stringLength = 140;
+const invalidValue = 'Максимальное значение не должно быть меньше или равно минимальному значению';
+const negativeValue = 'Диапазон может быть только положительный';
 
-const descriptionLikes = [];
-for (let index = MIN_NUMBER_LIKE; index <= MAX_NUMBER_LIKE; index ++) {
-  descriptionLikes.push(index);
-}
+const urlsAvatars = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
+];
 
-const commentsAvatarFotos = [];
-for (let index = 1; index <= QUANTITY_AVATAR_FOTOS; index ++) {
-  commentsAvatarFotos.push(index);
-}
 const commentsMessages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -75,15 +43,28 @@ const descriptions = [
   'Надейтесь на лучшее, но не ждите этого.',
 ];
 
-const getRandomArrayElement = (elements) => elements[getRandomValue(0, elements.length - 1)];
-const getNumber = (elements) => {
-  const total = 0;
-  const firstArrayElementIndex = 0;
-  for (let index = 0; index <= elements.length; index++) {
-    const delit = elements.splice(total, 1);
-    return elements[total], delit[firstArrayElementIndex];
+const getLengthRow = function (row, maxLength) {
+
+  if (row.length <= maxLength) {
+    return true;
   }
+  return false;
 };
+getLengthRow(ourString, stringLength);
+
+const getRandomValue = function (minValue, maxValue) {
+  if (minValue >= maxValue) {
+    return invalidValue;
+  }
+  if (minValue < 0 || maxValue < 0) {
+    return negativeValue;
+  }
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomValue(0, elements.length - 1)];
+
+const numberOfMessage = getRandomValue(MIN_QUANTITY_COMMENTS, MAX_QUANTITY_COMMENTS);
 
 const commentIdNumbers = [];
 const getRandomNumberIdComments = (min, max) => {
@@ -95,21 +76,21 @@ const getRandomNumberIdComments = (min, max) => {
     return number;
   }
 };
-const createFotoDescription = () => {
-  const numberOfComments = getRandomValue(MIN_QUANTITY_COMMENTS, MAX_QUANTITY_COMMENTS);
+const createFotoDescription = (index) => {
+
   let messageText = getRandomArrayElement(commentsMessages);
-  if (numberOfComments === 2) {
+  if (numberOfMessage === 2) {
     messageText += getRandomArrayElement(commentsMessages);
   }
   return {
-    id: getNumber(descriptionId),
-    url: 'photos/getNumber(descriptionUrl).jpg',
+    id: index,
+    url: 'photos/index.jpg',
     description: getRandomArrayElement(descriptions),
-    likes: getRandomArrayElement(descriptionLikes),
+    likes: getRandomValue(MIN_NUMBER_LIKE, MAX_NUMBER_LIKE),
     comments: [
       {
         id: getRandomNumberIdComments(MIN_COMMENT_ID, MAX_COMMENT_ID),
-        avatar: 'img/avatar-getRandomArrayElement(commentsAvatarFotos).svg',
+        avatar: getRandomArrayElement(urlsAvatars),
         message: messageText,
         name: getRandomArrayElement(names),
       },
@@ -117,6 +98,11 @@ const createFotoDescription = () => {
   };
 };
 
-const similarFotos = new Array(QUANTITY_GENERATED_OBJECTS).fill(null).map(() => createFotoDescription());
-console.log(similarFotos);
+const similarFotos = [];
+
+for (let index = 1; index <= QUANTITY_GENERATED_OBJECTS; index ++) {
+  const newRandomObjekt = createFotoDescription(index);
+  similarFotos.push(newRandomObjekt);
+}
+
 
