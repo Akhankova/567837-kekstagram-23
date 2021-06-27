b1e2ee989b7ab2b0df347504c9e9237adc4ad256
Alina Akhankova <a.akhankova@gmail.com>
2021-06-27 12:45:00 +0200

    Added a class to the picture

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,14 +1,20 @@
-//import {getArrayObject} from './main.js';
+//import { getRandomNumberIdComments } from "./util";
 
-
+const step = 5;
+const socialCommentCount = document.querySelector('.social__comment-count');
+const commentsCount = document.querySelector('.comments-count');
 const socialСomments = document.querySelector('.social__comments');
+const socialСommentsLoader = document.querySelector('.comments-loader');
 const getSocialComment = (elements) => {
   socialСomments.innerHTML = ' ';
   const similarListFragment = document.createDocumentFragment();
   elements.forEach((element) => {
     const socialComment = document.createElement('li');
     socialComment.classList.add('social__comment');
+
+    socialComment.classList.add('hidden');
     const socialCommentImg = document.createElement('img');
+    socialCommentImg.classList.add('social__picture');
     socialCommentImg.src = element.avatar;
     socialCommentImg.alt = element.name;
     socialCommentImg.width = '35';
@@ -21,8 +27,35 @@ const getSocialComment = (elements) => {
     similarListFragment.appendChild(socialComment);
   });
   socialСomments.appendChild(similarListFragment);
+  socialСommentsLoader.classList.remove('hidden');
+  for (let conter = 0; conter < step; conter++) {
+    if (socialСomments.children.item(conter)) {
+      socialСomments.children.item(conter).classList.add('visi');
+      socialСomments.children.item(conter).classList.remove('hidden');
+    }
+  }
+  const commentVisi = document.querySelectorAll('.visi');
+  let next = commentVisi[commentVisi.length-1].nextElementSibling;
+  let stepOfComment = 1;
+  const getMoreComments = () => {
+    while (stepOfComment <= step) {
+      if (next && next !== socialСomments.children[socialСomments.children.length-1]) {
+        next.classList.add('visi');
+        next.classList.remove('hidden');
+        next = next.nextElementSibling;
+        stepOfComment ++;
+      } else if (next === socialСomments.children[socialСomments.children.length-1]) {
+        next.classList.add('visi');
+        next.classList.remove('hidden');
+        socialСommentsLoader.classList.add('hidden');
+        break;
+      }else {
+        break;
+      }
+    }
+  };
+  socialСommentsLoader.addEventListener('click', getMoreComments);
 };
-
 
 const getBigPictures = (element) => {
   const bigPictureImg = document.querySelector('.big-picture__img');
@@ -33,4 +66,24 @@ const getBigPictures = (element) => {
   getSocialComment(element.comments);
   bigFoto.querySelector('.social__caption').textContent = element.description;
 };
+
+/*const getMore = () => {
+  socialСommentsLoader.classList.add('hidden');
+};
+socialСommentsLoader.addEventListener('click', getMore);
+socialСommentsLoader.classList.remove('hidden');
+
+else if (next === socialСomments.children[socialСomments.children.length-1]) {
+        next.classList.add('visi');
+        next.classList.remove('hidden');
+        socialCommentCount.textContent = commentsCount.textContent + span + isi + span + commentsCount.textContent + span + text;
+        socialСommentsLoader.classList.add('hidden');
+        break;
+      }
+
+          const span = ' ';
+    const text = 'комментариев';
+    const isi = 'из';
+*/
+
 export {getBigPictures};
