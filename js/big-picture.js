ce5ea18ecd8f6735137715bb6a5efd0eab951067
Alina Akhankova <a.akhankova@gmail.com>
2021-07-01 18:23:13 +0200

    Finished the module with the rendering of a big picture

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,78 +1,58 @@
 //import { getRandomNumberIdComments } from "./util";
 
-const step = 5;
 const socialCommentCount = document.querySelector('.social__comment-count');
 //const commentsCount = document.querySelector('.comments-count');
 const socialСomments = document.querySelector('.social__comments');
 const socialСommentsLoader = document.querySelector('.comments-loader');
 const getSocialComment = (elements) => {
-  let number = 5;
-  socialСomments.innerHTML = ' ';
-  const similarListFragment = document.createDocumentFragment();
-  elements.forEach((element) => {
-    const socialComment = document.createElement('li');
-    socialComment.classList.add('social__comment');
-    socialComment.classList.add('hidden');
-    const socialCommentImg = document.createElement('img');
-    socialCommentImg.src = element.avatar;
-    socialCommentImg.alt = element.name;
-    socialCommentImg.width = '35';
-    socialCommentImg.height = '35';
-    socialComment.appendChild(socialCommentImg);
-    const socialCommentP = document.createElement('p');
-    socialCommentP.classList.add('social__text');
-    socialCommentP.innerHTML = element.message;
-    socialComment.appendChild(socialCommentP);
-    similarListFragment.appendChild(socialComment);
-  });
-  socialСomments.appendChild(similarListFragment);
   socialСommentsLoader.classList.remove('hidden');
-  for (let conter = 0; conter < step; conter++) {
-    if (socialСomments.children.item(conter)) {
-      socialСomments.children.item(conter).classList.add('show');
-      socialСomments.children.item(conter).classList.remove('hidden');
-    }
-    if (socialСomments.children.item(conter) === socialСomments.children.item(socialСomments.children.length - 1)) {
-      socialСommentsLoader.classList.add('hidden');
-      socialCommentCount.textContent = `${socialСomments.children.length} из ${socialСomments.children.length} комментариев`;
-    }
-  }
-  const commentShow = document.querySelectorAll('.show');
-  let next = commentShow[commentShow.length-1].nextElementSibling;
-  const getMoreComments = () => {
-    let stepOfComment = 1;
-    while (stepOfComment <= step) {
-      if (next && next !== socialСomments.children[socialСomments.children.length-1]) {
-        next.classList.add('show');
-        next.classList.remove('hidden');
-        next = next.nextElementSibling;
-        stepOfComment ++;
-        if (stepOfComment === step-1) {
-          number += 5;
-          socialCommentCount.textContent = `${number} из ${socialСomments.children.length} комментариев`;
-        }
-      } else if (next === socialСomments.children[socialСomments.children.length-1]) {
-        next.classList.add('show');
-        next.classList.remove('hidden');
+  const similarListFragment = document.createDocumentFragment();
+  const secondListFragment = document.createDocumentFragment();
+  let commentsToShowCount = 0;
+  let quantityComments = 0;
+  const showComments = () => {
+    socialСomments.innerHTML = ' ';
+    commentsToShowCount += 5;
+    quantityComments += 5;
+    const commentsToShow = elements.slice(0, commentsToShowCount);
+    commentsToShow.forEach((element) => {
+      socialСomments.innerHTML = ' ';
+      const socialComment = document.createElement('li');
+      socialComment.classList.add('social__comment');
+      const socialCommentImg = document.createElement('img');
+      socialCommentImg.src = element.avatar;
+      socialCommentImg.alt = element.name;
+      socialCommentImg.width = '35';
+      socialCommentImg.height = '35';
+      socialComment.appendChild(socialCommentImg);
+      const socialCommentP = document.createElement('p');
+      socialCommentP.classList.add('social__text');
+      socialCommentP.innerHTML = element.message;
+      socialComment.appendChild(socialCommentP);
+      secondListFragment.appendChild(socialComment);
+      if (element === elements[elements.length-1]) {
         socialСommentsLoader.classList.add('hidden');
-        socialCommentCount.textContent = `${socialСomments.children.length} из ${socialСomments.children.length} комментариев`;
-        break;
-      }else {
-        break;
+        socialCommentCount.innerHTML = `${elements.length} из <span class='comments-count'>${elements.length}</span> комментариев`;
+      } else {
+        socialСommentsLoader.classList.remove('hidden');
+        socialCommentCount.innerHTML = `${quantityComments} из <span class='comments-count'>${elements.length}</span> комментариев`;
       }
-    }
+    });
+    socialСomments.appendChild(secondListFragment);
   };
-  socialСommentsLoader.addEventListener('click', getMoreComments);
+  showComments();
+  socialСommentsLoader.addEventListener('click', showComments);
+  socialСomments.appendChild(similarListFragment);
 };
+
 const getBigPictures = (element) => {
   const bigPictureImg = document.querySelector('.big-picture__img');
   const bigFoto = document.querySelector('.big-picture');
   bigPictureImg.querySelector('img').src = element.url;
   bigFoto.querySelector('.likes-count').textContent = element.likes;
-  bigFoto.querySelector('.comments-count').innerHTML = element.comments.length;
+  document.querySelector('.comments-count').textContent = element.comments.length;
   getSocialComment(element.comments);
   bigFoto.querySelector('.social__caption').textContent = element.description;
 };
 
 export {getBigPictures};
-
